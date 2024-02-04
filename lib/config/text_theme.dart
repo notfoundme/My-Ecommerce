6081776b3a338cd1b4675316f73dae66f48ad8ce
notfoundme <garimabhattarai059@gmail.com>
2024-02-04 21:07:53 +0545

    Added Product Model and Section title

--- lib/config/text_theme.dart
+++ lib/config/text_theme.dart
@@ -8,37 +8,37 @@ ThemeData theme() {
   );
 }
 
-
- TextTheme textTheme(){
+TextTheme textTheme() {
   return const TextTheme(
-   headlineLarge:TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
-                 ),
-                 headlineMedium:TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                 ),
-                 headlineSmall:TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                 ),
-                 bodyLarge:TextStyle(
-                  color: Color.fromRGBO(0, 0, 0, 1),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                 ),
-                 bodyMedium:TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                 ),bodySmall:TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 10,
-                 ),
-                 );
- }
+    headlineLarge: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontSize: 24,
+    ),
+    headlineMedium: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontSize: 18,
+    ),
+    headlineSmall: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    bodyLarge: TextStyle(
+      color: Color.fromRGBO(0, 0, 0, 1),
+      fontWeight: FontWeight.bold,
+      fontSize: 18,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 12,
+    ),
+    bodySmall: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 10,
+    ),
+  );
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_with_max/models/category_model.dart';
 import 'package:ecommerce_with_max/widgets/custom_navbar.dart';
 import 'package:ecommerce_with_max/widgets/hero_carousel_card.dart';
+import 'package:ecommerce_with_max/widgets/section_title.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/custom_appbar.dart';
@@ -25,24 +26,32 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         appBar: CustomAppBar(title: 'MY Ecommerce'),
         bottomNavigationBar: BottomBarWidget(),
-        body: Container(
-            child: CarouselSlider(
-          options: CarouselOptions(
-            //gives the height of the highlighted image
-            aspectRatio: 1.5,
-            //gives width to the highlighted image
-            viewportFraction: 0.9,
-            enlargeCenterPage: true,
-            enableInfiniteScroll: true,
-            //CenterPageEnlargeStrategy is used to reduce the space between the cards
-            enlargeStrategy: CenterPageEnlargeStrategy.height,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    //gives the height of the highlighted image
+                    aspectRatio: 1.5,
+                    //gives width to the highlighted image
+                    viewportFraction: 0.9,
+                    enlargeCenterPage: true,
+                    enableInfiniteScroll: true,
+                    //CenterPageEnlargeStrategy is used to reduce the space between the cards
+                    enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  ),
+                  //harek categoryModel lai tyo carousel card ma pass gardeko
+                  items: CategoryModel.categories
+                      .map((eachItemOfCategoryModel) => HeroCarouselCard(
+                          categoryModel: eachItemOfCategoryModel))
+                      .toList(),
+                ),
+              ),
+              SectionTitle(title: "RECOMMENDED")
+            ],
           ),
-          //harek categoryModel lai tyo carousel card ma pass gardeko
-          items: CategoryModel.categories
-              .map((eachItemOfCategoryModel) =>
-                  HeroCarouselCard(categoryModel: eachItemOfCategoryModel))
-              .toList(),
-        )),
+        ),
       ),
     );
   }
